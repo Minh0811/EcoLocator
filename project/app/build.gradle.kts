8cd9bb8623a499aab1faf191604573523b548bc2
minh0811 <vokhaiminh0811@gmail.com>
2023-12-08 17:18:05 +0700

    build gragle setting

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application")
+    kotlin("android") // Kotlin Android Plugin
+    kotlin("kapt")    // Kotlin Annotation Processor
 }
 
 android {
@@ -44,4 +46,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- project/build.gradle.kts
+++ project/build.gradle.kts
@@ -1,4 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.1.2" apply false
+// Project-level build.gradle.kts
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.5.1") // Android Gradle Plugin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31") // Replace with your Kotlin version
+    }
+}
+
+allprojects {
+    repositories {
+    }
 }
