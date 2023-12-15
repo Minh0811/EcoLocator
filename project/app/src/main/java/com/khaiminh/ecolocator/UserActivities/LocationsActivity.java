1437410b0c64ce2c869b71af63c713479254d068
minh0811 <vokhaiminh0811@gmail.com>
2023-12-16 03:04:58 +0700

    Set up google api and the google map in the project

--- project/app/src/main/java/com/khaiminh/ecolocator/UserActivities/LocationsActivity.java
+++ project/app/src/main/java/com/khaiminh/ecolocator/UserActivities/LocationsActivity.java
@@ -2,12 +2,19 @@ package com.khaiminh.ecolocator.UserActivities;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.khaiminh.ecolocator.R;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LocationsActivity extends AppCompatActivity {
-
+public class LocationsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +34,19 @@ public class LocationsActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        // Initialize map fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
