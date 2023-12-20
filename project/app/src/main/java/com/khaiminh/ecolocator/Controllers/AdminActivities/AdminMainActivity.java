53e0016e2402081505d984199af3c749c9e67cbd
minh0811 <vokhaiminh0811@gmail.com>
2023-12-20 19:14:04 +0700

    Add site recycler in admin page

--- project/app/src/main/java/com/khaiminh/ecolocator/Controllers/AdminActivities/AdminMainActivity.java
+++ project/app/src/main/java/com/khaiminh/ecolocator/Controllers/AdminActivities/AdminMainActivity.java
@@ -10,14 +10,25 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.khaiminh.ecolocator.Controllers.Authentication.LoginActivity;
+import com.khaiminh.ecolocator.Controllers.UserActivities.SiteAdapter;
 import com.khaiminh.ecolocator.R;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.khaiminh.ecolocator.Models.Site;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminMainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     Button button;
     TextView textView;
     FirebaseUser user;
+    private RecyclerView recyclerViewSites;
+    private SiteAdapter siteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +36,13 @@ public class AdminMainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         button = findViewById(R.id.logout);
-        textView = findViewById(R.id.admin_details);
         user = auth.getCurrentUser();
 
-        if(user == null){
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
+        textView = findViewById(R.id.admin_details);
+        if (user != null) {
             textView.setText(user.getEmail());
         }
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +53,27 @@ public class AdminMainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        // Setup RecyclerView
+        recyclerViewSites = findViewById(R.id.recyclerViewSites);
+        recyclerViewSites.setLayoutManager(new LinearLayoutManager(this));
+        siteAdapter = new SiteAdapter(new ArrayList<>()); // Initialize with an empty list
+        recyclerViewSites.setAdapter(siteAdapter);
+
+        loadSites(); // Method to load sites from Firestore
+    }
+    private void loadSites() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("locations") // Replace with your actual collection name
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    List<Site> sites = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                        sites.add(document.toObject(Site.class)); // Assuming a Site model class
+                    }
+                    siteAdapter.updateSites(sites);
+                })
+                .addOnFailureListener(e -> {
+                    // Handle failure
+                });
     }
 }
